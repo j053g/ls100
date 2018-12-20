c8e69f4c93690908ca3a6abf95db1e3abd0ec4dd
j053g <>
2018-12-20 07:34:39 -0500

    chapter_02: exercise 4

--- chapter_02_variables/name.rb
+++ chapter_02_variables/name.rb
@@ -1,8 +1,8 @@
 #Write a program called name.rb that asks the user to type in their name
 #and then prints out a greeting message with their name included.
 
-puts "What's your name"
-user_name = gets.chomp
-10.times do |i|
-  puts "Hello #{user_name}"
-end
+puts "What's your first name"
+first_name = gets.chomp
+puts "What's your last name"
+last_name = gets.chomp
+puts "Hello #{first_name} #{last_name}"
